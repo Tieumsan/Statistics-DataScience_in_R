f69bbfa2854624bc20518b15eb821db7509a6a6b
Jerimat <ml.legrand@outloo.com>
2020-06-05 23:27:51 +0200

    Intro to R - Swirl completed

--- intro_to_R/intro_to_R.R
+++ intro_to_R/intro_to_R.R
@@ -16,3 +16,21 @@ b <- rep(c(0, 1, 2), each=3)
 
 tf <- z%%2 == 0
 paste(z, c("X", "Y", "Z", "W"), sep = "")
+
+vect_names <- c('foo', 'bar', 'norf', 'foobar')
+names(z) <- vect_names
+z[-c(2, 4)] ## select all except indexes 2, 4
+
+my_vector <- c(1:20)
+dim(my_vector) <- c(4, 5)
+
+my_matrix <- matrix(1:20, nrow = 4, ncol = 5)
+identical(my_vector, my_matrix)
+
+bros <- c('Mat', 'Il', 'Al', 'In')
+my_data <- data.frame(cnames, my_data)
+cnames <- c('bro', 'age', 'weight', 'bp', 'rating', 'test')
+colnames(my_data) <- cnames
+my_data
+
+res <- students2 %>% gather(sex_class, count, -grade) %>% separate(sex_class, c('sex', 'class'))
